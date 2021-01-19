f835318b81d0a526a424c54ccbca40d391ae3a90
Martin PELCAT <martinpelcat@hotmail.com>
2021-01-19 12:51:53 +0100

    add types

--- singleton/src/services/CalendarService.ts
+++ singleton/src/services/CalendarService.ts
@@ -1,13 +1,14 @@
 import { Database } from "../utils/Database";
 
 export default class CalendarService {
-  public async getCurentDatabaseDate() {
+  public async getCurentDatabaseDate(): Promise<string> {
     const client = Database.getClient();
     try {
       const { rows } = await client.query("SELECT now()");
       return rows[0].now;
     } catch (error) {
       console.log(error);
+      throw new Error("Failed");
     }
   }
 }
