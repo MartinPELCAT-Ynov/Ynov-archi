67b0bbe4c05b9f8ae11bd80c15646ce436381ff9
Martin PELCAT <martinpelcat@hotmail.com>
2021-01-19 12:57:27 +0100

    change connect

--- singleton/src/index.ts
+++ singleton/src/index.ts
@@ -2,14 +2,16 @@ import CalendarService from "./services/CalendarService";
 import { Database } from "./utils/Database";
 
 (async () => {
-  await Database.getClient().connect();
+  const client = Database.getClient();
+  await client.connect();
+
   const calService = new CalendarService();
   const date = await calService.getCurentDatabaseDate();
-
   console.log(date);
 
   const calService2 = new CalendarService();
   const date2 = await calService2.getCurentDatabaseDate();
-
   console.log(date2);
+
+  await client.end();
 })();
