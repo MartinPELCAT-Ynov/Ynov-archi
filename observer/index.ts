4ceb0a987a9cda2583abbe41c739bbaa672c9208
Martin PELCAT <martinpelcat@hotmail.com>
2021-01-21 12:41:19 +0100

    add chalk

--- observer/index.ts
+++ observer/index.ts
@@ -1,6 +1,7 @@
 import { PhoneObserver } from "./src/observers/PhoneObserver";
 import { ProductSubject } from "./src/subjects/ProductSubject";
 
+// Notification
 const subject = new ProductSubject();
 const newPhoneObserver = new PhoneObserver();
 
@@ -13,3 +14,18 @@ subject.buyProduct(26); // 54
 subject.buyProduct(25); // 29
 subject.buyProduct(10); // 19
 subject.buyProduct(10); // 9
+
+// Pas de notification
+const subject2 = new ProductSubject();
+const newPhoneObserver2 = new PhoneObserver();
+
+subject.attach(newPhoneObserver2);
+
+//Trigger message when stock <= 10 , start : 100
+
+subject2.buyProduct(20); // 80
+subject2.buyProduct(26); // 54
+subject2.buyProduct(25); // 29
+subject2.buyProduct(10); // 19
+subject2.detach(newPhoneObserver2);
+subject2.buyProduct(10); // 9
--- observer/src/observers/PhoneObserver.ts
+++ observer/src/observers/PhoneObserver.ts
@@ -1,10 +1,13 @@
+import chalk from "chalk";
 import { IObserver } from "../interfaces/IObserver";
 import { ISubject } from "../interfaces/ISubject";
 
 export class PhoneObserver implements IObserver {
   update(subject: ISubject): void {
     if (subject.getStock() <= 10) {
-      console.log(`/!\\ Attention stock faible : ${subject.getStock()}`);
+      console.log(
+        chalk.red(`/!\\ Attention stock faible : ${subject.getStock()}`)
+      );
     }
   }
 }
