77da5af437e450a3ac911a9e5a811451ccd96f1a
Martin PELCAT <martinpelcat@hotmail.com>
2021-01-22 11:33:44 +0100

    regex

--- exercices/POO/Email.ts
+++ exercices/POO/Email.ts
@@ -8,6 +8,7 @@ export class Email implements IValidator {
   }
 
   validate(): void {
+    //Regex from stackoverflow
     const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailRegex.test(this.value)) {
       throw new Error("Email format invalid");
